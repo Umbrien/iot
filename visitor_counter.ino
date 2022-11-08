55dd58ee2b44bfa990dd740e35fac25fdfee229c
Umbrien <kalugin.serej@gmail.com>
2022-11-08 02:20:16 +0200

    add pinout

--- visitor_counter.ino
+++ visitor_counter.ino
@@ -1,5 +1,23 @@
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
+#include <LiquidCrystal_I2C.h> // https://github.com/johnrickman/LiquidCrystal_I2C
+
+// pinout
+//
+// left YL-63
+// OUT - D6
+// GND - GND
+// VCC - 3V3
+//
+// right YL-63
+// OUT - D5
+// GND - GND
+// VCC - 3V3
+//
+// I2C atached to 1602
+// GND - GND
+// VCC - Vin
+// SDA - D2
+// SCL - D1
+
 
 LiquidCrystal_I2C LCD (0x27, 16, 2);
 
@@ -12,7 +30,6 @@ LiquidCrystal_I2C LCD (0x27, 16, 2);
 void setup() {
   pinMode(L_YL63, INPUT);
   pinMode(R_YL63, INPUT);
-
 
   LCD.init();
   LCD.backlight();
@@ -28,5 +45,5 @@ void loop() {
   LCD.setCursor(2, 1);
   LCD.print(isRempty);
    
-  delay(500);
+   delay(500);
 }
